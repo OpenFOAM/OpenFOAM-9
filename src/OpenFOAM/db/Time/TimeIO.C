eca6d201d3db996a06a8c3c30ea7e6aed36c2eec
Henry Weller <http://cfd.direct>
2022-01-14 17:10:52 +0000

    Time: Improved the time directory precision changes for variable time-step cases
    Resolves bug-report https://bugs.openfoam.org/view.php?id=3781

--- src/OpenFOAM/db/Time/TimeIO.C
+++ src/OpenFOAM/db/Time/TimeIO.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -110,6 +110,7 @@ void Foam::Time::readDict()
     }
 
     controlDict_.readIfPresent("timePrecision", precision_);
+    curPrecision_ = precision_;
 
     // stopAt at 'endTime' or a specified value
     // if nothing is specified, the endTime is zero
