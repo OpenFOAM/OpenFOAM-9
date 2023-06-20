513812bc91e84090cfecef750a3c74e62b5c6d5f
Henry Weller <http://cfd.direct>
2023-06-20 19:17:18 +0100

    ISstream: Corrected parsing of variables

--- applications/test/dictionary/Test-dictionary.C
+++ applications/test/dictionary/Test-dictionary.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -48,13 +48,12 @@ int main(int argc, char *argv[])
 
     {
         dictionary dict;
-        dict.add("aaOPENMPIcc", 1);
+        dict.add("aaOpenFOAMcc", 1);
 
-        string s("DDD${aa${WM_MPLIB}cc}EEE");
+        string s("DDD${aa${WM_PROJECT}cc}EEE");
         stringOps::inplaceExpand(s, dict, true, false);
         Info<< "variable expansion:" << s << endl;
     }
-
 
     Info<< nl
         << "FOAM_CASE=" << getEnv("FOAM_CASE") << nl
